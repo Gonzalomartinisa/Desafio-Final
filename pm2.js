da27f5896054d23c1db8fd30e851ff55b5127995
Gonzalomartinisa <gonzalomartionnisa@gmail.com>
2022-10-24 17:32:55 -0300

    make it better

--- pm2.js
+++ pm2.js
@@ -1,12 +1,12 @@
-module.exports = {
-    apps : [{
-      name   : "server",
-      script : "server.js",
-      env: {
-        "NODE_ENV": "development",
-      },
-      env_production : {
-         "NODE_ENV": "production"
-      }
-    }]
-  }
+// module.exports = {
+//     apps : [{
+//       name   : "server",
+//       script : "server.js",
+//       env: {
+//         "NODE_ENV": "development",
+//       },
+//       env_production : {
+//          "NODE_ENV": "production"
+//       }
+//     }]
+//   }
